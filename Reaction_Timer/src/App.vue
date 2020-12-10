262978084d2440b27c747af74e4ddd58a574d831
Ganesh kolavennu <50847468+macklark@users.noreply.github.com>
2020-12-10 22:32:34 +0530

    main component

---
+++ Reaction_Timer/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class = "title">
+      <h1>{{ title }}</h1>
+    </div>
+    <div class = "btn">
+    <button @click = "startHandler">Go</button>
+    </div>
+    <Playground :delay = "delay" v-if = "isPlaying"/>
+</template>
+
+<script>
+  import Playground from "./components/Playground"
+  export default{
+    name : "App",
+    components : {
+      Playground
+    },
+    data(){
+      return{
+        title : "Reaction Timer",
+        isPlaying : false,
+        delay : null
+      }
+    },
+    methods : {
+      startHandler(){
+        this.delay = 2000 + Math.random() * 5000
+        this.isPlaying = true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title{
+    text-align: center;
+    font-family: 'Nerko One', cursive;
+    color: #35495e ;
+  }
+
+  .btn{
+    text-align: center;
+  }
+
+  .btn button{
+    width: 60px;
+    height: 30px;
+    cursor: pointer;
+  }
+</style>
